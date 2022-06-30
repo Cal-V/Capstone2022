b8d0c3b2b6b6e0e7c23fe974f8921ee12da046d3
Cal V <kallenkozuki314@gmail.com>
2022-06-29 19:10:26 -0600

    Making the reminder text show up italicised

---
+++ deckbuilder/src/Components/DetailedCard/CardDetails/DetailBlock/OracleText/OracleLine.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Symbol from '../Symbol/Symbol'
+
+function OracleLine({line,reminder}) {
+
+    const symbolRegex = /\{[A-Z/0-9∞½]+\}/g
+    const textRegex = /[A-Z/0-9∞½]+(?=})/g
+
+    const oracleTextSymbols = line[0].match(textRegex) || []
+    const oracleTextWords = line[0].split(symbolRegex) || []
+
+    return (
+        <div className="inline">
+            {oracleTextWords.map((segment,segmentIndex) => (
+                <div key={segmentIndex} className="inline">
+                    {reminder ? 
+                        <div className='inline'><i className='inline'>{segment}</i>{(oracleTextSymbols[segmentIndex] != null ? <Symbol shadow={false} symbol={oracleTextSymbols[segmentIndex]} /> : "" )}</div>
+                    :
+                        <div className='inline'><p className='inline'>{segment}</p>{(oracleTextSymbols[segmentIndex] != null ? <Symbol shadow={false} symbol={oracleTextSymbols[segmentIndex]} /> : "" )}</div>
+                    }
+                    
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default OracleLine
--- deckbuilder/src/Components/DetailedCard/CardDetails/DetailBlock/OracleText/OracleText.js
+++ deckbuilder/src/Components/DetailedCard/CardDetails/DetailBlock/OracleText/OracleText.js
@@ -1,35 +1,27 @@
 import React from 'react'
-import Symbol from '../Symbol/Symbol'
+import OracleLine from './OracleLine'
 
 function OracleText({card_text}) {
-
-    const symbolRegex = /\{[A-Z/0-9∞½]+\}/g
-    const textRegex = /[A-Z/0-9∞½]+(?=})/g
     
     //split text into each line
     const oracleTextLines = card_text.split("\n")
+    
+    const iRegex = /(\([+{},/:. 'A-Za-z/0-9∞½-]+\))/g
 
-    //splitting each line of each face on the symbols [line][words]
-    const oracleTextWords = oracleTextLines.map(line => (
-        line.split(symbolRegex)
+    const oracleTextGroups = oracleTextLines.map((line) => (
+        line.split(iRegex)
     ))
-
-    //getting each of the symbols [line][symbol]
-    const oracleTextSymbols = oracleTextLines.map(line => (
-        line.match(textRegex)
+    const oracleTextFlavors = oracleTextLines.map(line => (
+        line.match(iRegex)
     ))
 
     return (
         <>
             {card_text.length > 0 ?
                 <>{
-                oracleTextWords.map((line,lineIndex) => (
+                oracleTextGroups.map((line,lineIndex) => (
                     <div key={lineIndex} className="oracle-text-holder">
-                    {line.map((segment,segmentIndex) => (
-                        <p key={segmentIndex} className="inline">
-                        <div dangerouslySetInnerHTML={{__html: segment}} className="inline"/>{(oracleTextSymbols[lineIndex]?.[segmentIndex] != null ? <Symbol shadow={false} symbol={oracleTextSymbols[lineIndex]?.[segmentIndex]} /> : "" )}
-                        </p>
-                    ))}
+                        <OracleLine line={line} reminder={false} className="inline" />{oracleTextFlavors[lineIndex] ? <OracleLine className="inline" line={oracleTextFlavors[lineIndex]} reminder={true}/> : <></>}
                     </div>
                 ))
             }</>
